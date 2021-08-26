79b4f5c540a04d6a7b8f54912fcab338fdaeaf2f
Tianxu Xi <xitianxu36@gmail.com>
2021-08-26 15:31:40 +1000

    change home page

--- src/main/java/com/payslipProgram/monthlyPayslip/controller/MainController.java
+++ src/main/java/com/payslipProgram/monthlyPayslip/controller/MainController.java
@@ -25,7 +25,7 @@ public class MainController {
 	
 	@GetMapping
 	public String home() {
-		return "This is the monthly payslip application with aws cdk, and this is the home page.";
+		return "This is the monthly payslip application with aws cdk, check health check.";
 	}
 
 	@PostMapping("/input")
